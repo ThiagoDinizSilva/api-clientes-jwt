92f05d62c7606572c85ec78632a461d9ae41bf28
Thiago Diniz <diniz.dev@hotmail.com>
2022-01-09 20:41:53 -0300

    Update SwaggerConfig.java

--- src/main/java/br/com/neoapp/mvpClientes/config/SwaggerConfig.java
+++ src/main/java/br/com/neoapp/mvpClientes/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package br.com.neoapp.mvpClientes.config;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
@@ -13,7 +14,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -21,18 +26,40 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableSpringDataWebSupport
 public class SwaggerConfig extends WebMvcConfigurationSupport {
+
 	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("br.com.neoapp")).build().apiInfo(metaData());
+	public Docket api() {
+	    return new Docket(DocumentationType.SWAGGER_2)
+	      .apiInfo(metaData())
+	      .securityContexts(Arrays.asList(securityContext()))
+	      .securitySchemes(Arrays.asList(apiKey()))
+	      .select()
+	      .apis(RequestHandlerSelectors.basePackage("br.com.neoapp"))
+	      .build();
 	}
 
 	private ApiInfo metaData() {
 		return new ApiInfoBuilder().title("Spring Boot NeoApp MVP").description("\"Teste de Habilidades NeoApp\"")
 				.version("1.0.0").license("Apache License Version 2.0")
-				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"").build();
+				.licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+				.build();
 	}
+	
+	private ApiKey apiKey() { 
+	    return new ApiKey("JWT", "Authorization", "header"); 
+	}
+	
+	private SecurityContext securityContext() { 
+	    return SecurityContext.builder().securityReferences(defaultAuth()).build(); 
+	} 
 
+	private List<SecurityReference> defaultAuth() { 
+	    AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything"); 
+	    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1]; 
+	    authorizationScopes[0] = authorizationScope; 
+	    return Arrays.asList(new SecurityReference("JWT", authorizationScopes)); 
+	}
+	
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(new PageableHandlerMethodArgumentResolver());
